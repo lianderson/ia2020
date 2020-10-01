1b9b7f5d7c828eb220403b1ec70a323bb137314b
Diego Cassolli dos Santos <cassolli@kinghost.com.br>
2020-09-30 21:54:58 -0300

    ajuste

--- entrega1/entrega3.py
+++ entrega1/entrega3.py
@@ -3,6 +3,6 @@ import Aulas.ia2020.entrega1.modulo as mod
 acoes = ['ITUB3','PNVL4','ABEV3']
 
 for i in acoes:
-    fonte=mod.getFontes('ABEV3')
+    fonte=mod.getFontes(i)
     noticia = mod.getHtml(fonte)
     mod.gravaArquivo("noticia"+str(i)+".txt",noticia)
