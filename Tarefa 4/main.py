d8f9d19d8e3f208ca71a8d99ce90e8f4ac8b31b4
sousasgabriel <48535460+sousasgabriel@users.noreply.github.com>
2020-10-07 20:57:57 -0300

    Ajuste entrega 4
    ajuste entrega 4

---
+++ Tarefa 4/main.py
@@ -0,0 +1,4 @@
+import t4
+
+acoes = ['LWSA3.SA']
+t4.buscaAcao(acoes)
--- Tarefa 4/t4.py
+++ Tarefa 4/t4.py
@@ -14,32 +14,31 @@ def fnYFinJSON(symbol):
     return yFinJSON["quoteResponse"]["result"][0]
 
 
+def buscaAcao(tickers):
 
-tickers = ["LWSA3.SA", "KLBN11.SA", "SLCE3.SA"]
-
-fields = {'shortName': 'Company', 'bookValue': 'Book Value', 'currency': 'Curr',
-                  'fiftyTwoWeekLow': '52W L', 'fiftyTwoWeekHigh': '52W H',
-                  'regularMarketPrice': 'Price',
-                  'regularMarketDayHigh': 'High', 'regularMarketDayLow': 'Low',
-                  'priceToBook': 'P/B', 'trailingPE': 'LTM P/E', 'regularMarketDayLow': 'DayLow',
-                  'regularMarketPrice': 'regularMarketPrice', 'regularMarketOpen': 'regularMarketOpen',
-                  'ask': 'close', 'regularMarketDayHigh': 'regularMarketDayHigh',
-                  'marketState': 'marketState', 'averageDailyVolume3Month': 'averageDailyVolume3Month',
-                  'regularMarketDayLow': 'regularMarketDayLow', 'fiftyDayAverage': 'fiftyDayAverage',
-                  'fiftyTwoWeekLow': 'fiftyTwoWeekLow', 'twoHundredDayAverage': 'twoHundredDayAverage',
-                  'fiftyTwoWeekHigh': 'fiftyTwoWeekHigh', 'regularMarketChange': 'regularMarketChange',
-                  'regularMarketChangePercent': 'regularMarketChangePercent', 'longName': 'longName'}
-results = {}
-for ticker in tickers:
-    tickerData = fnYFinJSON(ticker)  # le o site
-    singleResult = {}
-    for key in fields.keys():
-        if key in tickerData:
-            singleResult[fields[key]] = tickerData[key]
-        else:
-            singleResult[fields[key]] = "N/A"
-    results[ticker] = singleResult
-    print("---------------------")
-    print(results[ticker]["close"])
-    print(results[ticker]["longName"])
+    fields = {'shortName': 'Company', 'bookValue': 'Book Value', 'currency': 'Curr',
+                      'fiftyTwoWeekLow': '52W L', 'fiftyTwoWeekHigh': '52W H',
+                      'regularMarketPrice': 'Price',
+                      'regularMarketDayHigh': 'High', 'regularMarketDayLow': 'Low',
+                      'priceToBook': 'P/B', 'trailingPE': 'LTM P/E', 'regularMarketDayLow': 'DayLow',
+                      'regularMarketPrice': 'regularMarketPrice', 'regularMarketOpen': 'regularMarketOpen',
+                      'ask': 'close', 'regularMarketDayHigh': 'regularMarketDayHigh',
+                      'marketState': 'marketState', 'averageDailyVolume3Month': 'averageDailyVolume3Month',
+                      'regularMarketDayLow': 'regularMarketDayLow', 'fiftyDayAverage': 'fiftyDayAverage',
+                      'fiftyTwoWeekLow': 'fiftyTwoWeekLow', 'twoHundredDayAverage': 'twoHundredDayAverage',
+                      'fiftyTwoWeekHigh': 'fiftyTwoWeekHigh', 'regularMarketChange': 'regularMarketChange',
+                      'regularMarketChangePercent': 'regularMarketChangePercent', 'longName': 'longName'}
+    results = {}
+    for ticker in tickers:
+        tickerData = fnYFinJSON(ticker)  # le o site
+        singleResult = {}
+        for key in fields.keys():
+            if key in tickerData:
+                singleResult[fields[key]] = tickerData[key]
+            else:
+                singleResult[fields[key]] = "N/A"
+        results[ticker] = singleResult
+        print("---------------------")
+        print(results[ticker]["close"])
+        print(results[ticker]["longName"])
     
