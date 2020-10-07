596be350242ae26849c0b09c94339b8c932ca817
JoaoVitor <joaokussler@gmail.com>
2020-10-07 20:34:51 -0300

    tarefa 4
    adicionado tarefa4

--- Tarefa 3/modulo1.py
+++ Tarefa 3/modulo1.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from googlesearch import search
 from goose3 import Goose
 
@@ -13,13 +14,38 @@ def get_simple_page(url):
     retorno += "\n---------------------"
     retorno += "\n" + noticia.cleaned_text
     retorno += "\nfim noticia\n\n\n"
+    g.close()
     return retorno
 
+def check_url(url):
+    retorno = 0
+    g = Goose()
+    noticia = g.extract(url=url)
+    if(noticia._publish_datetime_utc):
+        agora_unf = datetime.now()
+        data_unf = noticia.publish_datetime_utc
+        agora = datetime.strptime(str(agora_unf.day)+"-"+str(agora_unf.month)+"-"+str(agora_unf.year), "%d-%m-%y")
+        data = datetime.strptime(str(data_unf.day)+"-"+str(data_unf.month)+"-"+str(data_unf.year), "%d-%m-%y")
+        tempoEntre = agora - data
+        if(tempoEntre.days <= 7):
+            retorno = 1
+    
+    return retorno
+
+"""
+ate_time_str = '2018-06-29 08:15:27.243860'
+date_time_obj = datetime.datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S.%f')
+
+print('Date:', date_time_obj.date())
+print('Time:', date_time_obj.time())
+print('Date-time:', date_time_obj)
+"""
 arr = ["lwsa3 noticia","slce3 noticia", "klbn4 noticia"]
 resultado = ""
 for url in arr:
-    for result in consulta(url, 5):
-        resultado += get_simple_page(result)
+    for result in consulta(url, 10):
+        if(check_url(result) == 1):
+            resultado += get_simple_page(result)
 
 file = open("Noticias.txt", "w") 
 file.write(resultado) 
