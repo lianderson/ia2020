447e456e5eb9c6380584c8b6273aef5c30e1d6fe
João Vitor <joaokussler@gmail.com>
2020-10-14 21:41:03 -0300

    Tarefa 5
    Criei T5 com as funcoes para inserir no banco as acoes

--- Tarefa 5/t5.py
+++ Tarefa 5/t5.py
@@ -1,17 +1,24 @@
+import schedule
+import time
+import pymysql
 import urllib.request  # pacote para trabalhar com mewb
 import json  # pacote para manipular JSON
-
+from datetime import datetime
+from decimal import Decimal
 
 def fnYFinJSON(symbol):
     urlData = "https://query2.finance.yahoo.com/v7/finance/quote?symbols=" + symbol
-    print(urlData)
+    #print(urlData)
     webUrl = urllib.request.urlopen(urlData)
     if (webUrl.getcode() == 200):
         data = webUrl.read()
     else:
         print("problema ao ler os resultado " + str(webUrl.getcode()))
     yFinJSON = json.loads(data)
-    return yFinJSON["quoteResponse"]["result"][0]
+    try:
+        return yFinJSON["quoteResponse"]["result"][0]
+    except:
+        return []
 
 
 def buscaAcao(tickers):
@@ -41,3 +48,36 @@ def buscaAcao(tickers):
         return str(results[ticker]["close"])
         
     
+
+def busca_cotacoes(equipe):
+    conexao = pymysql.connect(host='viajuntos.com.br', user='admin_ia', passwd='admin_ia',db='admin_ia')
+    cursor_banco = conexao.cursor()
+    sql = "SELECT * FROM acao WHERE id_equipe = " + str(equipe)
+    cursor_banco.execute(sql)
+    conexao.close()
+    return cursor_banco
+
+def salva_cotacoes_atuais(sigla, id):
+    now = datetime.now()
+    acao = [sigla]
+    preco = Decimal(buscaAcao(acao))
+    data_formatada = now.strftime('%Y-%m-%d %H:%M:%S')
+    conexao = pymysql.connect(host='viajuntos.com.br', user='admin_ia', passwd='admin_ia',db='admin_ia')
+    cursor_banco = conexao.cursor()
+    cursor_banco.execute('insert into cotacao(equipe_id,preco,data_importacao,acao_id) values(%s, %s, %s, %s)', (3, preco, data_formatada,id))
+    conexao.commit()
+    conexao.close()
+
+
+def atualiza_cotacoes_equipe3():
+    cotacoes = busca_cotacoes(3)#busca cotacoes da equipe com id 3
+    for row in cotacoes:
+        salva_cotacoes_atuais(str(row[1]), str(row[0]))
+
+    #loop pelas cotacoes
+
+#schedule.every(.1).minutes.do(rodar)
+
+
+atualiza_cotacoes_equipe3()
+print("feito")
