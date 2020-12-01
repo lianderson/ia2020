9a97fa2942b73aeaf6a5d595c31ae44c60ecbeae
Mauricio <mauricio.zaquia@hotmail.com>
2020-11-30 23:29:42 -0300

    Robo para identificar compra ou venda

---
+++ 20201202 Decima segunda entrega/decimaSegundaEntrega.py
@@ -0,0 +1,136 @@
+# ===============================================================================
+# inteligencia artificial
+# data: 2020 10 07
+# script: desenvolver script parar pesquisar as informacoes das empresas utiolizando api do yahoo
+# autores: mauricio zaquia, lindice lopes, gustavo berté
+# ===============================================================================
+
+# ===============================================================================
+# importando as bibliotecas
+# ===============================================================================
+import moduloDecimaSegundaEntrega as mqe
+import schedule
+import time
+import pymysql
+from datetime import datetime
+import sys
+
+# ===============================================================================
+# criacao de variaveis
+# ===============================================================================
+caminho = 'D:\\Projetos\\python\\InteligenciaArtificial\\#github\\ia2020\\20201021 Sexta entrega\\log_cotacoes.txt'
+
+arrayInformacoes = []
+arrayAcoes = []
+equipe_id = 0
+acao_id = 0
+acao_preco_atual = 0
+acao_simbolo = ''
+decisao = ''
+
+# ===============================================================================
+# criando a funcao para rodar a busca
+# ===============================================================================
+
+
+def rodarBusca():
+    try:
+        # limpando o arrayacoes (lista) para nao duplicar as cotacoes
+        arrayAcoes = []
+        arrayAnalise = []
+        # ===============================================================================
+        # conexao com o banco
+        # ===============================================================================
+        conexao = pymysql.connect(host='viajuntos.com.br', user='admin_ia', password='admin_ia', db='admin_ia')
+        # ===============================================================================
+        # fazendo o select das acoes
+        # ===============================================================================
+        cursor_banco = conexao.cursor()
+        sql = "SELECT * FROM acao WHERE id_equipe = 2"
+        cursor_banco.execute(sql)
+        for linhas in (cursor_banco.fetchall()):
+            arrayAcoes.append(linhas)
+        cursor_banco.close()    
+        # ===============================================================================
+        # lendo os dados
+        # ===============================================================================
+        for a in (arrayAcoes):
+            arrayAnalise=[]
+            acao_id = a[0]
+            acao_simbolo = a[1]
+            equipe_id = a[2]
+
+            arrayInformacoes = mqe.retornaCamposFormatados(acao_simbolo)
+            acao_preco_atual = arrayInformacoes[acao_simbolo]['Preço Atual']
+
+            # ===============================================================================
+            # busca informacoes da analise
+            # ===============================================================================
+            conexao = pymysql.connect(host='viajuntos.com.br', user='admin_ia', password='admin_ia', db='admin_ia')
+            cursor_banco = conexao.cursor()
+            sql = 'SELECT media FROM equipe2_analise WHERE acao_id = %s' % (acao_id)
+            cursor_banco.execute(sql)            
+            for b in (cursor_banco.fetchall()):
+                arrayAnalise.append(b)
+            cursor_banco.close()
+
+            for c in(arrayAnalise):                
+                v_analise_media = c[0]
+
+            if (acao_preco_atual < float(v_analise_media)):
+                decisao = ''
+                decisao = 'comprar'
+                print(acao_simbolo + " Preco " + str(acao_preco_atual) + " menor que a media " + str(v_analise_media) + ", comprar!")
+            elif (acao_preco_atual > float(v_analise_media)):
+                decisao = ''
+                decisao = 'vender'
+                print(acao_simbolo + " Preco " + str(acao_preco_atual) + " maior que a media " + str(v_analise_media) + ", vender!")
+            else:                
+                decisao = ''
+                decisao = 'neutro'
+                print(acao_simbolo + " Preco " + str(acao_preco_atual) + " igual a media " + str(v_analise_media) + ", neutro!")
+
+            # ===============================================================================
+            # inserindo a cotacao na tabela cotacao no banco
+            # ===============================================================================
+            cursor_banco = conexao.cursor()
+            sql = 'INSERT INTO equipe2_robo(valor_compra,valor_venda,acao_id,equipe_id,valor_atual,decisao) values(%s,%s,%s,%s,%s,"%s") ' % (acao_preco_atual,acao_preco_atual,acao_id,equipe_id,acao_preco_atual,decisao)
+            cursor_banco.execute(sql)
+            conexao.commit()
+            cursor_banco.close()
+
+            print(acao_simbolo + ': Registro inserido em: ' + str(datetime.now()))
+            mqe.gravaLog(caminho, acao_simbolo +': Registro inserido em: ' + str(datetime.now()) + '\n')
+
+        if (conexao.open):
+            conexao.close()
+    except ValueError:
+        print("Não foi possível converter")
+    except BufferError:
+        print("Buffer Cheio")
+    except IOError:
+        print("IOError")
+    except:
+        print("Erro sem tratamento:", sys.exc_info()[0])
+    finally:
+        if (conexao.open):
+            conexao.close()
+        mqe.gravaLog(caminho, 'Executado em: ' + str(datetime.now()) + '\n')
+
+
+# ===============================================================================
+# configuracao para rodar a busca
+# ===============================================================================
+schedule.every(.1).minutes.do(rodarBusca)
+# schedule.every().hour.do(rodarBusca) #descomentar para pegar os valores de hora e hora
+
+now = datetime.now()
+hora = str(now.hour) + str(now.minute)
+
+while (1):
+    rodarBusca()
+    # if (hora > '1000') and (hora < '2400'):
+    # schedule.run_pending()
+    # time.sleep(1)
+    #now = datetime.now()
+    #hora = str(now.hour) + str(now.minute)
---
+++ 20201202 Decima segunda entrega/moduloDecimaSegundaEntrega.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+# ===============================================================================
+# importando as bibliotecas
+# ===============================================================================
+import urllib.request  # pacote para trabalhar com mewb
+import json  # pacote para manipular JSON
+from goose3 import Goose
+from googlesearch import search
+from datetime import datetime
+import pymysql
+
+# ===============================================================================
+# criando a funcao para retornar json valores passando o codigo da acao por parametro
+# ===============================================================================
+
+
+def fnYFinJSON(stock):
+    urlData = "https://query2.finance.yahoo.com/v7/finance/quote?symbols=" + stock
+    # print(urlData)
+    webUrl = urllib.request.urlopen(urlData)
+    if (webUrl.getcode() == 200):
+        data = webUrl.read()
+    else:
+        print("problema ao ler os resultado " + str(webUrl.getcode()))
+    yFinJSON = json.loads(data)
+    return yFinJSON["quoteResponse"]["result"][0]
+
+
+# ===============================================================================
+# criando a funcao para separar os campos
+# ===============================================================================
+def retornaCamposFormatados(pEmpresa):
+
+    empresa = pEmpresa
+
+    tickers = [empresa]
+
+    fields = {'shortName': 'Empresa',
+              'bookValue': 'Válor Contábil',
+              'currency': 'Moeda',
+              'fiftyTwoWeekLow': '52W L',
+              'fiftyTwoWeekHigh': '52W H',
+              'regularMarketPrice': 'Preço Atual',
+              'regularMarketDayHigh': 'Alta do Dia',
+              'regularMarketDayLow': 'Baixa do Dia',
+              'priceToBook': 'P/L Proporção',
+              'trailingPE': 'Índice P/L',
+              'regularMarketDayLow': 'DayLow',
+              'regularMarketPrice': 'Preço Atual',
+              'regularMarketOpen': 'Preço na Abertura',
+              'ask': 'Preço de Venda',
+              'regularMarketDayHigh': 'Alta do Dia',
+              'marketState': 'Estado do Mercado',
+              'averageDailyVolume3Month': 'Estimativa Volume',
+              'regularMarketDayLow': 'Baixa do Dia',
+              'fiftyDayAverage': 'Média da V52',
+              'fiftyTwoWeekLow': 'V52 Baixa',
+              'twoHundredDayAverage': 'Média da Variação de 200 Dias',
+              'fiftyTwoWeekHigh': 'V52 Alta',
+              'regularMarketChange': 'Diferença',
+              'regularMarketChangePercent': 'Diferença Porcentagem',
+              'longName': 'Nome Completo',
+              'symbol': 'Simbolo'}
+
+    results = {}
+
+    for ticker in tickers:
+        tickerData = fnYFinJSON(ticker)  # le o site
+        singleResult = {}
+        for key in fields.keys():
+            if key in tickerData:
+                singleResult[fields[key]] = tickerData[key]
+            else:
+                singleResult[fields[key]] = "N/A"
+        results[ticker] = singleResult
+
+    # print(results)
+    # print(results[empresa]['Empresa']);
+    #print(results[empresa]['Preço Atual']);
+
+    #preco_atual                = results[empresa]['Preço Atual'];
+    #preco_abertura             = results[empresa]['Preço na Abertura'];
+    #preco_baixa                = results[empresa]['Baixa do Dia'];
+    #preco_venda                      = results[empresa]['Preço de Venda'];
+    #preco_alta                 = results[empresa]['Alta do Dia'];
+    #estado_mercado                = results[empresa]['Estado do Mercado'];
+    #estimativa_volume   = results[empresa]['Estimativa Volume'];
+    #media_52            = results[empresa]['Média da V52'];
+    #media_52B            = results[empresa]['V52 Baixa'];
+    #media_52A           = results[empresa]['V52 Alta'];
+    #media_200       = results[empresa]['Média da Variação de 200 Dias'];
+    #diferenca        = results[empresa]['Diferença'];
+    #diferenca_por100 = results[empresa]['Diferença Porcentagem'];
+    #nome_completo                   = results[empresa]['Nome Completo'];
+    # print(nome_completo)
+
+    return results
+
+# ===============================================================================
+# criando a funcao para pesquisar no google search
+# ===============================================================================
+
+
+def retornaURL(consulta):
+    lista = search(consulta, num_results=5, lang="pt-br")
+    return lista
+
+# ===============================================================================
+# criando a funcao para separar URL
+# ===============================================================================
+
+
+def retornaInformacoesSite(site_url):
+    g = Goose()
+    noticia = g.extract(url=site_url)
+
+    arrayNoticia = [noticia.title, noticia.cleaned_text, noticia.publish_date]
+
+    return(arrayNoticia)
+
+
+def verificaNoticiaDuplicada(pCursor, url_noticia):
+    cont = 0
+    
+    sql2 = "SELECT * FROM noticias WHERE url_noticia = '%s'" % (url_noticia)
+    pCursor.execute(sql2)
+    for duplicados in (pCursor.fetchall()):
+        cont = cont + 1
+    
+    return cont
+
+def gravaLog(caminho, mensagem):
+    f = open(caminho, 'a')
+    f.write(mensagem)
+    f.close()
